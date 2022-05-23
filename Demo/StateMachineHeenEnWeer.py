e5d6f45890eda30fd760c6e302e0ec2a763cfff7
Joep <jsuijs@gmail.com>
2022-05-23 20:14:41 +0200

    Support for sub-statemachines & better reporting.

--- Demo/StateMachineHeenEnWeer.py
+++ Demo/StateMachineHeenEnWeer.py
@@ -3,23 +3,28 @@ from microbit import *
 from MaqueenApi import *
 from Robot import *
 
-def StateWachtA(S):
-    if S.NewState :
+def SequenceWachtA(S):  # Sequence naam betekent de eerste state van een serie
+    if S.IsNewState('SequenceWachtA') :
         RGB(1, 1)
 
     if pin5.read_digital() == False:
         S.Goto(StateWacht1s)
 
 def StateWacht1s(S):
-    if S.NewState :
+    if S.IsNewState('StateWacht1s') :
         RGB(2, 2)
 
     if S.StateTime(1000) :
-        S.Goto(RijVoorwaards)
+        S.IsDone = True # einde van sequence
     return
 
-def RijVoorwaards(S):
-    if S.NewState :
+def SequenceHeenEnWeer(S):
+    if S.IsNewState('SequenceHeenEnWeer') :
+        pass
+    S.GoSub(SequenceWachtA, StateRijVoorwaards);
+
+def StateRijVoorwaards(S):
+    if S.IsNewState('StateRijVoorwaards') :
         RGB(4,4)
         motor(1, 100, 1, 100)
 
@@ -28,19 +33,17 @@ def RijVoorwaards(S):
     if S.StateTime(5000) :
         RGB(0,0)
         motor(1, 0, 1, 0)
-        S.Goto(StateDone)
+        S.IsDone = True
     return
 
 # ------------------------------------------------------------------------------
 # start van main
 # ------------------------------------------------------------------------------
-Sm = StateMachine()
-
-Sm.Goto(StateWachtA)
+Sm = StateMachine(SequenceHeenEnWeer)
 
 print("Begin")
 # voer statemachine uit zolang deze nog niet 'Done' is
-while Sm.Done == False:
+while Sm.IsDone == False:
     Sm.Takt()
 
 print("Einde")
--- Libs/Robot.py
+++ Libs/Robot.py
@@ -1,29 +1,48 @@
 import utime
 
-def StateDone(S):
-   S.Done = True
-
-
 class StateMachine:
 
-   def __init__(self):
-      self.ActiveState = StateDone
-      self.NewState = True
-      self.Done = False
+   def __init__(self, StartState):
+      self._ActiveState = StartState
+      self._NewState    = True
+      self._SubS        = None
+      self.IsDone       = False
       pass
 
+   def IsNewState(self, StateName):
+      if self._NewState:
+         print("NewState", StateName)
+      return self._NewState
+
    def Goto(self, NextState):
-      print("Goto", NextState)
-      self.StartMs = utime.ticks_ms()
-      self.PreNewState = True
-      self.ActiveState = NextState
+      print("Goto")
+      self._StartMs     = utime.ticks_ms()
+      self._PreNewState = True
+      self._ActiveState = NextState
+
+   def GoSub(self, ViaState, NextState):
+      print("GoSub")
+      self._SubS        = StateMachine(ViaState)
+      self._NextState   = NextState
 
    def Takt(self):
-        self.Done = False
-        self.PreNewState = False
-        self.ActiveState(self)
-        self.NewState = self.PreNewState
-        return self.Done
+      if self._SubS != None:
+         # handle sub-statemachine until it completes
+         self._SubS.Takt()
+         if self._SubS.IsDone == False:
+            return
+
+         # Sub Done -> next for this StateMachine
+         self._SubS = None;
+         self.Goto(self._NextState)
+         self._NewState = True # required to start NextState properly
+
+      # Work on this StateMachine
+      self.IsDone = False
+      self._PreNewState = False
+      self._ActiveState(self)
+      self._NewState = self._PreNewState
+      return self.IsDone
 
    def StateTime(self, Delay):
-      return (utime.ticks_ms() - (self.StartMs + Delay)) > 0
+      return (utime.ticks_ms() - (self._StartMs + Delay)) > 0
